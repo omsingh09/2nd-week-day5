3a3277dfa3bf3f16a9e5b16aa427430c60a251e0
omsingh09 <94465259+omsingh09@users.noreply.github.com>
2023-10-28 20:11:31 +0530

    Add files via upload

---
+++ Body.jsx
@@ -0,0 +1,87 @@
+import React from "react";
+import "./Body.css"
+const Body =()=>{
+    return(
+        <>
+        <hr />
+        <div class="boydiv">
+            <div class="single">
+            <h1 class="singletitle"> Single <br></br> Web Hosting</h1>
+            <h4 class="ideal">Ideal solution for begineers</h4>
+            <br></br>
+        <br></br>
+        <br></br>
+        <h3 class="dol">$</h3>
+        <h2 class="number">29</h2>
+        <h5 class="per">/mo</h5>
+        <button class="sbutton">
+            Add to Cart
+        </button>
+        <h6 class="buttonbot">159/mo when you renew</h6>
+        <h3 class="web"> 1 Website </h3>
+        <h3 class="visit"> ~10000 Visits Monthly </h3>
+        <h3 class="gb"> 30 GB SSD Storage</h3>
+        <h3 class="hgb">100 GB Bandwidth</h3>
+        <h1 class="features">See all features</h1>
+        <img  class="wrong1" src="images\OIP__1_-removebg-preview.png" alt=""></img>
+        <img  class="wrong2" src="images\OIP__1_-removebg-preview.png" alt=""></img>
+        <img  class="right1" src="images\13-138883_clipart-tick-correct-removebg-preview.png" alt=""></img>
+        <img  class="right2" src="images\13-138883_clipart-tick-correct-removebg-preview.png" alt=""></img>
+
+
+            </div>
+        <div class="ultimate">
+        <h1 class="webhosting"> Ultimate <br></br> Web Hosting</h1>
+        <h4 class="perk">Prefect package for business</h4>
+        <br></br>
+        <br></br>
+        <br></br>
+        <h3 class="dola">$</h3>
+        <h2 class="number2">99</h2>
+        <h5 class="per2">/mo</h5>
+        <button class="sbutton2">
+            Add to Cart
+        </button>
+        <h6 class="buttonbot2">159/mo when you renew</h6>
+        <h3 class="web2"> 100 Website </h3>
+        <h3 class="visit2"> ~75000 Visits Monthly </h3>
+        <h3 class="gb2"> 200 GB SSD Storage</h3>
+        <h3 class="hgb2">500 GB Bandwidth</h3>
+        <h1 class="features">See all features</h1>
+        <img  class="right3" src="images\13-138883_clipart-tick-correct-removebg-preview.png" alt=""></img>
+        <img  class="right4" src="images\13-138883_clipart-tick-correct-removebg-preview.png" alt=""></img>
+        <img  class="right5" src="images\13-138883_clipart-tick-correct-removebg-preview.png" alt=""></img>
+        <img  class="right6" src="images\13-138883_clipart-tick-correct-removebg-preview.png" alt=""></img>
+      </div>
+
+      <div class="ultimate3">
+        <h1 class="webhosting3"> Premium <br></br>Web Hosting</h1>
+        <h4 class="perk3">Prefect package for personal</h4>
+        <br></br>
+        <br></br>
+        <br></br>
+        <h3 class="dola3">$</h3>
+        <h2 class="number3">59</h2>
+        <h5 class="per3">/mo</h5>
+        <button class="sbutton3">
+            Add to Cart
+        </button>
+        <h6 class="buttonbot3">159/mo when you renew</h6>
+        <h3 class="web3"> 50 Website </h3>
+        <h3 class="visit3"> ~25000 Visits Monthly </h3>
+        <h3 class="gb3"> 60 GB SSD Storage</h3>
+        <h3 class="hgb3">200 GB Bandwidth</h3>
+        <h1 class="features">See all features</h1>
+        <img  class="right7" src="images\13-138883_clipart-tick-correct-removebg-preview.png" alt=""></img>
+        <img  class="right8" src="images\13-138883_clipart-tick-correct-removebg-preview.png" alt=""></img>
+        <img  class="right9" src="images\13-138883_clipart-tick-correct-removebg-preview.png" alt=""></img>
+        <img  class="bestval" src="images\Capture-removebg-preview.png" alt=""></img>
+        <img  class="wrong3" src="images\OIP__1_-removebg-preview.png" alt=""></img>
+      </div>
+
+        </div>
+        </>
+    );
+};
+
+export default Body;
